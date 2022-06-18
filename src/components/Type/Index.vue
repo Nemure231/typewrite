e93206635450e86440a7e27e609b807dc98d598a
Nemure231 <neruhi5321@gmail.com>
2022-06-18 20:42:30 +0700

    fix: double word if type so fast

--- src/components/Type/Index.vue
+++ src/components/Type/Index.vue
@@ -13,37 +13,186 @@ const { a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, 
 const storeAbjad = ref([])
 const storedAbjadJoin = ref([])
 
+
 watchEffect(() => {
     if (start.value) {
         a.value && storeAbjad.value.push('a')
-        b.value && storeAbjad.value.push('b')
-        c.value && storeAbjad.value.push('c')
-        d.value && storeAbjad.value.push('d')
-        e.value && storeAbjad.value.push('e')
-        f.value && storeAbjad.value.push('f')
-        g.value && storeAbjad.value.push('g')
-        h.value && storeAbjad.value.push('h')
-        i.value && storeAbjad.value.push('i')
-        j.value && storeAbjad.value.push('j')
-        k.value && storeAbjad.value.push('k')
-        l.value && storeAbjad.value.push('l')
-        m.value && storeAbjad.value.push('m')
-        n.value && storeAbjad.value.push('n')
-        o.value && storeAbjad.value.push('o')
-        p.value && storeAbjad.value.push('p')
-        q.value && storeAbjad.value.push('q')
-        r.value && storeAbjad.value.push('r')
-        s.value && storeAbjad.value.push('s')
-        t.value && storeAbjad.value.push('t')
-        u.value && storeAbjad.value.push('u')
-        v.value && storeAbjad.value.push('v')
-        w.value && storeAbjad.value.push('w')
-        x.value && storeAbjad.value.push('x')
-        y.value && storeAbjad.value.push('y')
-        z.value && storeAbjad.value.push('z')
-        Minus.value && storeAbjad.value.push('-')
-        space.value && storeAbjad.value.push(' ')
+    }
+})
 
+
+watchEffect(() => {
+    if (start.value) {
+        b.value && storeAbjad.value.push('b')
+    }
+})
+
+
+watchEffect(() => {
+    if (start.value) {
+        c.value && storeAbjad.value.push('c')
+    }
+})
+
+watchEffect(() => {
+    if (start.value) {
+        d.value && storeAbjad.value.push('d')
+    }
+})
+
+
+watchEffect(() => {
+    if (start.value) {
+        e.value && storeAbjad.value.push('e')
+    }
+})
+
+
+watchEffect(() => {
+    if (start.value) {
+        f.value && storeAbjad.value.push('f')
+    }
+})
+
+watchEffect(() => {
+    if (start.value) {
+        g.value && storeAbjad.value.push('g')
+    }
+})
+
+watchEffect(() => {
+    if (start.value) {
+        h.value && storeAbjad.value.push('h')
+    }
+})
+
+watchEffect(() => {
+    if (start.value) {
+        i.value && storeAbjad.value.push('i')
+    }
+})
+
+
+
+watchEffect(() => {
+    if (start.value) {
+        j.value && storeAbjad.value.push('j')
+    }
+})
+
+watchEffect(() => {
+    if (start.value) {
+        k.value && storeAbjad.value.push('k')
+    }
+})
+
+
+watchEffect(() => {
+    if (start.value) {
+        l.value && storeAbjad.value.push('l')
+    }
+})
+
+watchEffect(() => {
+    if (start.value) {
+        m.value && storeAbjad.value.push('m')
+    }
+})
+
+watchEffect(() => {
+    if (start.value) {
+        n.value && storeAbjad.value.push('n')
+    }
+})
+
+watchEffect(() => {
+    if (start.value) {
+        o.value && storeAbjad.value.push('o')
+    }
+})
+
+watchEffect(() => {
+    if (start.value) {
+        p.value && storeAbjad.value.push('p')
+    }
+})
+
+watchEffect(() => {
+    if (start.value) {
+        q.value && storeAbjad.value.push('q')
+    }
+})
+
+watchEffect(() => {
+    if (start.value) {
+        r.value && storeAbjad.value.push('r')
+    }
+})
+
+watchEffect(() => {
+    if (start.value) {
+        s.value && storeAbjad.value.push('s')
+    }
+})
+
+watchEffect(() => {
+    if (start.value) {
+        t.value && storeAbjad.value.push('t')
+    }
+})
+
+watchEffect(() => {
+    if (start.value) {
+        u.value && storeAbjad.value.push('u')
+    }
+})
+
+watchEffect(() => {
+    if (start.value) {
+        v.value && storeAbjad.value.push('v')
+    }
+})
+
+watchEffect(() => {
+    if (start.value) {
+        w.value && storeAbjad.value.push('w')
+    }
+})
+
+watchEffect(() => {
+    if (start.value) {
+        x.value && storeAbjad.value.push('x')
+    }
+})
+
+watchEffect(() => {
+    if (start.value) {
+        y.value && storeAbjad.value.push('y')
+    }
+})
+
+
+watchEffect(() => {
+    if (start.value) {
+        z.value && storeAbjad.value.push('z')
+    }
+})
+
+
+watchEffect(() => {
+    if (start.value) {
+        Minus.value && storeAbjad.value.push('-')
+    }
+})
+
+watchEffect(() => {
+    if (start.value) {
+        space.value && storeAbjad.value.push(' ')
+    }
+})
+
+watchEffect(() => {
+    if (start.value) {
         if (Backspace.value) {
             if (storeAbjad.value.length == 1) {
                 storeAbjad.value = []
@@ -51,12 +200,21 @@ watchEffect(() => {
                 storeAbjad.value.pop()
             }
         }
+    }
+})
 
-        if(Delete.value){
+watchEffect(() => {
+    if (start.value) {
+        if (Delete.value) {
             storeAbjad.value = [];
         }
-        var stored = storeAbjad.value.join('')
+    }
+})
 
+
+watchEffect(() => {
+    if (start.value) {
+        var stored = storeAbjad.value.join('')
         previewType.value = stored
 
         if (storeAbjad.value.length > 0) {
@@ -106,9 +264,11 @@ watchEffect(() => {
             }
         }
 
+
+
     }
 
-})
+}, { immediate: true })
 
 
 
