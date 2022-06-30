f3a753cc0c8aa7b7a567b200e05ceadfcb1f0917
Nemure231 <neruhi5321@gmail.com>
2022-06-30 22:32:15 +0700

    fix: bg dark in pwa notif

--- src/components/Reload/Index.vue
+++ src/components/Reload/Index.vue
@@ -20,21 +20,21 @@ export default defineComponent({
 });
 </script>
 <template>
-    <div v-memo="[offlineReady, needRefresh]"  v-if="offlineReady || needRefresh"
-        class="fixed right-3 bottom-3 text-left p-4 rounded-lg shadow-lg z-50 dark:bg-secondary bg-light border dark:border-light/25 border-secondary/25" role="alert">
+    <div  v-if="offlineReady || needRefresh"
+        class="fixed right-3 dark:text-gray-300 text-black bottom-3 text-left p-4 rounded-lg shadow-lg z-50 dark:bg-gray-800 bg-white border dark:border-gray-300/25 border-secondary/25" role="alert">
         <div class="mb-3">
-            <span class="font-semibold text-primary dark:text-light" v-if="offlineReady">
+            <span class="font-semibold" v-if="offlineReady">
                 App ready to install
             </span>
-            <span class="font-semibold text-primary dark:text-light" v-else>
+            <span class="font-semibold" v-else>
                 New content available, please reload your app.
             </span>
         </div>
-        <button class="py-0 px-4 bg-light dark:bg-dark dark:border  text-primary dark:text-light rounded-xl border-2 border-primary/50 dark:border-light/50" v-if="needRefresh"
+        <button class="py-0 px-4 bg-gray-300 dark:bg-gray-800 dark:border rounded-xl border-2 border-sky-500/50 dark:border-gray-300/50" v-if="needRefresh"
             @click="updateServiceWorker()">
             Reload
         </button>
-        <button class="py-0 px-4 bg-light dark:bg-dark dark:border text-primary dark:text-light rounded-xl border-2 border-primary/50 dark:border-light/50" @click="close">
+        <button class="py-0 px-4 bg-gray-300 dark:bg-gray-800 dark:borde rounded-xl border-2 border-sky-500/50 dark:border-gray-300/50" @click="close">
             Close
         </button>
     </div>
